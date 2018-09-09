9599f245b2c8d6acbc5586057cef666578ac1058
srafi1 <shakilrafi0@gmail.com>
2018-09-09 01:40:36 -0400

    load animation

--- static/js/script.js
+++ static/js/script.js
@@ -1,19 +1,50 @@
-$('document').ready(() => $('#idea').keydown(function(e){
-  if(e.which==13){
-    console.log('entered');
+var idea = '';
 
-    var text = $(this).val();
+$('document').ready(() => {
 
-    $.ajax({
-      url: "/search",
-      type: "get",
-      data: {jsdata: text},
-      success: function(response) {
-        console.log(response);
-        $("#place_for_suggestions").html(response);
-      },
-      error: function(xhr) {
+  $('#idea').keydown(function(e){
+    if(e.which==13){
+      console.log('entered idea');
+      $('#idea').prop('disabled', true);
+      $('#results').children().first().fadeOut('slow', function() {
+        $(this).html('Analyzing your idea').fadeIn('slow');
+      });
+      $('#results').children().fadeIn();
+
+      idea = $(this).val();
+
+      $.ajax({
+        url: "/search",
+        type: "get",
+        data: {jsdata: idea},
+        success: animateIdea,
+        error: function(xhr) {
+        }
+      });
+
+      return false;
+    };
+  });
+});
+
+function animateIdea(response) {
+  $('#results').children().fadeOut('fast', function() {
+    $(this).remove();
+  });
+
+  var results = d3.select('#results');
+  var svg = results.append('svg').attr('id', 'idea-animation');
+  var words = idea.split(' ');
+  var coloredSentence = '';
+  words.forEach((word) => {
+      if (word === 'asdf') {
+        coloredSentence +=  '<span class="blue-text">' + word + '</span>';
+      } else {
+        coloredSentence +=  word;
       }
     });
-  };
-}));
+  svg.append('text')
+    .attr('x', 0)
+    .attr('y', 50)
+    .html(coloredSentence);
+}
