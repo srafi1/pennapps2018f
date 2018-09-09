d24b4e06349cb9f4fd4a8d167e140ab4c3d09fe8
MdAbedin <mdabedincs@gmail.com>
2018-09-08 20:19:21 -0400

    text similarity works

--- app.py
+++ app.py
@@ -1,10 +1,47 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from algoliasearch import algoliasearch
+import requests
+import unirest
+import json
 
 app = Flask(__name__)
+
+client = algoliasearch.Client("EKBPNS72VM", 'f9c93e7e2ca6d13410b38757e0aec21e')
+
+submissions = dict()
+with open('data.json') as f:
+    submissions = json.load(f)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/search')
+def search():
+    text = request.args.get('jsdata')
+    print text
+    if text:
+        similar = []
+
+        for submission in submissions:
+            if submission['info'] != '':
+                response = unirest.post("https://twinword-text-similarity-v1.p.mashape.com/similarity/",
+                                        headers={
+                                            "X-Mashape-Key": "yl3VcenMlfmshPcSaR0A21BJS82Sp1Mvq8LjsnkzSXBXvwXfVg",
+                                            "Content-Type": "application/x-www-form-urlencoded",
+                                            "Accept": "application/json"
+                                        },
+                                        params={
+                                            "text1": text,
+                                            "text2": submission['info']
+                                        }
+                )
+
+
+                if response.body['similarity'] > 0.3:
+                    similar.append(submission['info'])
+
+    return render_template('suggestions.html', suggestions = similar)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,19 @@
 $('document').ready(() => $('#idea').keydown(function(e){
-    if(e.which==13){
-        console.log('entered');
-        return false;
-    }
+  if(e.which==13){
+    console.log('entered');
+
+    var text = $(this).val();
+
+    $.ajax({
+      url: "/search",
+      type: "get",
+      data: {jsdata: text},
+      success: function(response) {
+        console.log(response);
+        $("#place_for_suggestions").html(response);
+      },
+      error: function(xhr) {
+      }
+    });
+  };
 }));
